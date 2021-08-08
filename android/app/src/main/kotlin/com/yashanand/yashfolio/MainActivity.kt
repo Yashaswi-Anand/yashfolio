4abef44598250ffe5909f701600ea12e41303e1c
Yashaswi Anand <yash.kwr17@gmail.com>
2021-08-08 12:48:10 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/yashanand/yashfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yashanand.yashfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yash',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Yash Anand"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Yashaswi Anand \n Computer Science Student"),
+        ),
+      ),
+    );
+  }
+}
